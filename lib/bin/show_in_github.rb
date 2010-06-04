8ed710bcd768f86e966511eec8aebbed0c755122
Max Stepanov <mstepanov@aptana.com>
2010-06-03 18:23:52 -0700

    [517-github-bundle-commands-that-suppose-to-open-a-browse-do-not-work-on-ubuntu-104-x64] use x-www-browser command to open urls on linux

--- lib/bin/show_in_github.rb
+++ lib/bin/show_in_github.rb
@@ -2,15 +2,18 @@
 
 $:.unshift(File.dirname(__FILE__) + "/..")
 require "rubygems"
+require "rbconfig"
 require "show_in_github"
 
 begin
   url = ShowInGitHub.url_for(ENV['TM_FILEPATH'])
   lines = ENV['TM_INPUT_START_LINE'] ? "#{ENV['TM_INPUT_START_LINE']}-#{ENV['TM_LINE_NUMBER']}" : ENV['TM_LINE_NUMBER']
-  if RUBY_PLATFORM.downcase =~ /(win|w)32$/
-    `explorer #{url}#L#{lines}`
-  else
+  if RbConfig::CONFIG['host_os'] =~ /win32|windows|mswin|mingw/
+    `start #{url}#L#{lines}`
+  elsif RbConfig::CONFIG['host_os'] =~ /darwin/
     `open #{url}#L#{lines}`
+  else
+    `x-www-browser #{url}#L#{lines}`    
   end
 rescue NotGitRepositoryError
   puts "File/project not a git repository"
